5176fa41fbb4587d55b137d9ef479f9eaebc0329
Dmitriy Kulyasov <vvvvvvmazday@gmai.com>
2022-01-31 22:01:40 +0300

    new chatbot

---
+++ SimpleBot.java
@@ -0,0 +1,18 @@
+package bot;
+
+import java.time.Year;
+import java.util.Scanner;
+
+public class SimpleBot {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello! My name is Aid.");
+        System.out.println("I was created in "+ Year.now());
+        System.out.println("Please, remind me your name.");
+
+        // reading a name
+        String name = scanner.nextLine();
+        System.out.printf("What a great name you have, %s!", name);
+    }
+}
