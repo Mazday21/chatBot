1f2005493d2479cb571901bfdb5b45e03ebe9b38
Dmitriy Kulyasov <vvvvvvmazday@gmai.com>
2022-02-02 21:16:37 +0300

    test commit

--- SimpleBot.java
+++ SimpleBot.java
@@ -10,20 +10,20 @@ public class SimpleBot {
         System.out.println("Hello! My name is Aid.");
         System.out.println("I was created in "+ Year.now());
         System.out.println("Please, remind me your name.");
-        System.out.println("> ");
+        System.out.print("> ");
 
         String name = scanner.nextLine();
 
         System.out.println("What a great name you have, " + name + "!");
         System.out.println("Let me guess your age.");
         System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
-        System.out.println("> ");
+        System.out.print("> ");
         int div3 = scanner.nextInt();
-        System.out.println("> ");
+        System.out.print("> ");
         int div5 = scanner.nextInt();
-        System.out.println("> ");
+        System.out.print("> ");
         int div7 = scanner.nextInt();
         int age = (div3 * 70 + div5 * 21 + div7 * 15) % 105;
-        System.out.printf("Your age is %d; that's a good time to start programming!", age);
+        System.out.println("Your age is "+age+"; that's a good time to start programming!");
     }
 }
