f063c5220dad5fa8a6b88a15bffef937310c7f68
Dmitriy Kulyasov <vvvvvvmazday@gmai.com>
2022-02-02 21:57:30 +0300

    + count

--- SimpleBot.java
+++ SimpleBot.java
@@ -4,17 +4,23 @@ import java.time.Year;
 import java.util.Scanner;
 
 public class SimpleBot {
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
+        hello();
+        age();
+        count();
+    }
+    public static void hello() {
         System.out.println("Hello! My name is Aid.");
-        System.out.println("I was created in "+ Year.now());
+        System.out.println("I was created in " + Year.now());
         System.out.println("Please, remind me your name.");
         System.out.print("> ");
 
         String name = scanner.nextLine();
 
         System.out.println("What a great name you have, " + name + "!");
+    }
+    public static void age() {
         System.out.println("Let me guess your age.");
         System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
         System.out.print("> ");
@@ -24,6 +30,15 @@ public class SimpleBot {
         System.out.print("> ");
         int div7 = scanner.nextInt();
         int age = (div3 * 70 + div5 * 21 + div7 * 15) % 105;
-        System.out.println("Your age is "+age+"; that's a good time to start programming!");
+        System.out.println("Your age is " + age + "; that's a good time to start programming!");
+    }
+    public static void count(){
+        System.out.println("Now I will prove to you that I can count to any number you want.");
+        System.out.print("> ");
+        int number = scanner.nextInt();
+        for (int i = 0; i <= number; i++) {
+            System.out.println(i+"!");
+            }
+        System.out.println("Completed, have a nice day!");
     }
 }
