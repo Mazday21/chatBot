d1e2e7c671c49ec41697e36ef1e6d59c96ae594c
Dmitriy Kulyasov <vvvvvvmazday@gmai.com>
2022-02-02 19:45:33 +0300

    test

--- SimpleBot.java
+++ SimpleBot.java
@@ -10,7 +10,7 @@ public class SimpleBot {
         System.out.println("Hello! My name is Aid.");
         System.out.println("I was created in "+ Year.now());
         System.out.println("Please, remind me your name.");
-
+        System.out.println("> ");
         // reading a name
         String name = scanner.nextLine();
         System.out.printf("What a great name you have, %s!", name);
